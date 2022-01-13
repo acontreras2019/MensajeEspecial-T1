d26d56d58605afec3968d4886ab6d18682094ebe
cmaradiaga <cristianmaradiaga39@gmail.com>
2022-01-13 08:39:10 -0600

    Se ha realizado el cambio a un solo mensaje en vez de 3 y se añadió una posdata.

--- MensajeEspecial/MensajeEspecial/Form1.cs
+++ MensajeEspecial/MensajeEspecial/Form1.cs
@@ -19,10 +19,11 @@ namespace MensajeEspecial
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Lo unico imposible es aquello que no intentas... para todo el equipo de CAHSA");
-            MessageBox.Show("Equipo IT CAHSA");
-            MessageBox.Show("Alejandrina Contreras Zavala");
-            
+            MessageBox.Show("Lo unico imposible es aquello que no intentas... para todo el equipo de CAHSA"
+                            + Environment.NewLine + "Equipo IT CAHSA"
+                            + Environment.NewLine + "Alejandrina Contreras Zavala"
+                            + Environment.NewLine + Environment.NewLine + "Pdt. Vamos a comer hoy");
+
 
         }
     }
